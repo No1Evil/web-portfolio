fa96bbb7903a09cc6354aa5e4ac49aafdff41cd1
NotEvil <notevil036@gmail.com>
2026-05-03 14:56:11 +0300

    feat: configured routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {About} from './features/about/about';
+import {Projects} from './features/projects/projects';
+import {Skills} from './features/skills/skills';
+import {Contact} from './features/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: About },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'skills', component: Skills },
+  { path: 'contact', component: Contact }
+];
