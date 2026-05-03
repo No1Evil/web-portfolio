6a7b385834f54fcf17932e804db46ba610a21f2c
NotEvil <notevil036@gmail.com>
2026-05-03 15:14:35 +0300

    fix: fix features tests

--- frontend/src/app/features/contact/contact.spec.ts
+++ frontend/src/app/features/contact/contact.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { Contact } from './contact';
 
@@ -9,6 +12,11 @@ describe('Contact', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Contact],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Contact);
--- frontend/src/app/features/projects/projects.spec.ts
+++ frontend/src/app/features/projects/projects.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { Projects } from './projects';
 
@@ -9,6 +12,11 @@ describe('Projects', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Projects],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Projects);
