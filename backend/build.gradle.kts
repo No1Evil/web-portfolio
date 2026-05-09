ec4a3fb06004d88226b43b0877419f7abde0e4e0
NotEvil <notevil036@gmail.com>
2026-05-09 04:04:18 +0300

    chore(backend): configure build.gradle.kts for multi module structure

---
+++ backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java-library")
+}
+
+allprojects {
+    apply(plugin = "java-library")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok:1.18.46")
+        annotationProcessor("org.projectlombok:lombok:1.18.46")
+
+        testCompileOnly("org.projectlombok:lombok:1.18.46")
+        testAnnotationProcessor("org.projectlombok:lombok:1.18.46")
+    }
+
+    tasks.withType<JavaCompile> {
+        options.release.set(21)
+    }
+
+    java {
+        toolchain {
+            JavaLanguageVersion.of(21)
+        }
+    }
+
+    javaToolchains {
+        version = JavaVersion.VERSION_21
+    }
+}
