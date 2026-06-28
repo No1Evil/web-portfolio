3668edc8f49401ec28b894f405ac81bb93081cc3
no1evil <notevil036@gmail.com>
2026-06-28 10:32:43 +0300

    chore(backend/application): remove Spring dependencies

--- backend/application/build.gradle.kts
+++ backend/application/build.gradle.kts
@@ -1,16 +1,9 @@
-plugins {
-    alias(libs.plugins.spring.boot)
-    alias(libs.plugins.spring.dependency.management)
-}
-
 description = "Application layer"
 
 dependencies {
     implementation(project(":domain"))
     implementation("io.github.robsonkades:uuidv7:1.0.1")
 
-    implementation("org.springframework.boot:spring-boot-starter")
-}
 
 tasks.bootJar {
     enabled = false
