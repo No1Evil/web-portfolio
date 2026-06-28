8f5993dd1791ba2278f800607058a9d1be1d2252
no1evil <notevil036@gmail.com>
2026-06-28 10:33:18 +0300

    chore(backend/application): add mapstruct, junit, mockito, assertj

--- backend/application/build.gradle.kts
+++ backend/application/build.gradle.kts
@@ -4,11 +4,16 @@ dependencies {
     implementation(project(":domain"))
     implementation("io.github.robsonkades:uuidv7:1.0.1")
 
+    implementation(libs.mapstruct)
+    annotationProcessor(libs.mapstruct.processor)
 
-tasks.bootJar {
-    enabled = false
+    testImplementation(platform("org.junit:junit-bom:6.1.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.15.2")
+    testImplementation("org.assertj:assertj-core:3.27.7")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.bootRun {
-    enabled = false
+tasks.test {
+    useJUnitPlatform()
 }
