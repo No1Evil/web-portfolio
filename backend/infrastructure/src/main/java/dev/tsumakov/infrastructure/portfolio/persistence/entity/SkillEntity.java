645d5422ee2589f10733eddc09b122315ccebbe1
no1evil <notevil036@gmail.com>
2026-07-20 23:06:38 +0300

    fix(infrastructure): SkillEntity table name corrected to user_skills

--- backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/portfolio/persistence/entity/SkillEntity.java
+++ backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/portfolio/persistence/entity/SkillEntity.java
@@ -13,7 +13,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "skills", schema = "portfolio")
+@Table(name = "user_skills", schema = "portfolio")
 @Getter
 @Setter
 public class SkillEntity {
