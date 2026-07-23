44f9b7a6a857911c9d1e2ca167f8a9c223b8d1a2
no1evil <notevil036@gmail.com>
2026-07-23 23:36:09 +0300

    feat(infrastructure): implement core.UseCaseConfig

---
+++ backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/core/config/UseCaseConfig.java
@@ -0,0 +1,56 @@
+package dev.tsumakov.infrastructure.core.config;
+
+import dev.tsumakov.application.core.mapper.RoleDtoMapper;
+import dev.tsumakov.application.core.mapper.UserDtoMapper;
+import dev.tsumakov.application.core.port.in.role.CreateRoleUseCase;
+import dev.tsumakov.application.core.port.in.role.DeleteRoleUseCase;
+import dev.tsumakov.application.core.port.in.role.GetAllRolesUseCase;
+import dev.tsumakov.application.core.port.in.user.CreateUserUseCase;
+import dev.tsumakov.application.core.port.in.user.GetAllUsersUseCase;
+import dev.tsumakov.application.core.port.in.user.GetUserByEmailUseCase;
+import dev.tsumakov.application.core.usecase.role.CreateRoleUseCaseImpl;
+import dev.tsumakov.application.core.usecase.role.DeleteRoleUseCaseImpl;
+import dev.tsumakov.application.core.usecase.role.GetAllRolesUseCaseImpl;
+import dev.tsumakov.application.core.usecase.user.CreateUserUseCaseImpl;
+import dev.tsumakov.application.core.usecase.user.GetAllUsersUseCaseImpl;
+import dev.tsumakov.application.core.usecase.user.GetUserByEmailUseCaseImpl;
+import dev.tsumakov.domain.core.repository.RoleRepository;
+import dev.tsumakov.domain.core.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCaseConfig {
+
+  @Bean
+  public CreateRoleUseCase createRoleUseCase(RoleRepository roleRepository, RoleDtoMapper roleDtoMapper) {
+    return new CreateRoleUseCaseImpl(roleRepository, roleDtoMapper);
+  }
+
+  @Bean
+  public DeleteRoleUseCase deleteRoleUseCase(RoleRepository roleRepository) {
+    return new DeleteRoleUseCaseImpl(roleRepository);
+  }
+
+  @Bean
+  public GetAllRolesUseCase getAllRolesUseCase(RoleRepository roleRepository, RoleDtoMapper roleDtoMapper) {
+    return new GetAllRolesUseCaseImpl(roleRepository, roleDtoMapper);
+  }
+
+  @Bean
+  public CreateUserUseCase createUserUseCase(UserRepository userRepository,
+      RoleRepository roleRepository, UserDtoMapper userDtoMapper)  {
+    return new CreateUserUseCaseImpl(userRepository, roleRepository, userDtoMapper);
+  }
+
+  @Bean
+  public GetAllUsersUseCase getAllUsersUseCase(UserRepository userRepository, UserDtoMapper userDtoMapper) {
+    return new GetAllUsersUseCaseImpl(userRepository, userDtoMapper);
+  }
+
+  @Bean
+  public GetUserByEmailUseCase getUserByEmailUseCase(UserRepository userRepository, UserDtoMapper userDtoMapper) {
+    return new GetUserByEmailUseCaseImpl(userRepository, userDtoMapper);
+  }
+
+}
