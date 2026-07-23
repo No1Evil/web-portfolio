7f2bf34243b06aadd3ce5d7762e80c587b1a0d0f
no1evil <notevil036@gmail.com>
2026-07-23 23:05:25 +0300

    feat(infrastructure): implement UserSpringDataRepository

---
+++ backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/core/persistence/repository/UserSpringDataRepository.java
@@ -0,0 +1,11 @@
+package dev.tsumakov.infrastructure.core.persistence.repository;
+
+import dev.tsumakov.infrastructure.core.persistence.entity.UserEntity;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSpringDataRepository extends JpaRepository<UserEntity, UUID> {
+
+}
