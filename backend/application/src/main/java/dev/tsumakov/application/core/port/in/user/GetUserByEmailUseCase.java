de05d6bd167eadd929d994e553fd76f28df483c0
no1evil <notevil036@gmail.com>
2026-08-03 15:42:36 +0300

    refactor(application): GetUserByEmailUseCase should not return optional

--- backend/application/src/main/java/dev/tsumakov/application/core/port/in/user/GetUserByEmailUseCase.java
+++ backend/application/src/main/java/dev/tsumakov/application/core/port/in/user/GetUserByEmailUseCase.java
@@ -1,8 +1,7 @@
 package dev.tsumakov.application.core.port.in.user;
 
 import dev.tsumakov.application.core.dto.api.UserDto;
-import java.util.Optional;
 
 public interface GetUserByEmailUseCase {
-  Optional<UserDto> execute(String email);
+  UserDto execute(String email);
 }
