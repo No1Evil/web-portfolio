93727f21ef0728d95f2bd02597f31f846752a0a3
no1evil <notevil036@gmail.com>
2026-08-17 17:31:28 +0300

    feat(db): add icon_url field to skills table

--- backend/infrastructure/src/main/resources/db/changelog/core/01-core.sql
+++ backend/infrastructure/src/main/resources/db/changelog/core/01-core.sql
@@ -29,6 +29,7 @@ create table if not exists core.skills
     id          integer generated always as identity primary key,
     category_id integer     references core.skill_categories (id) on delete set null,
     name        varchar(50) not null,
+    icon_url    text,
     created_at  timestamptz not null default current_timestamp,
     updated_at  timestamptz not null default current_timestamp,
     version     bigint      not null default 1,
