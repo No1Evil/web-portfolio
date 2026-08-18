2cf99d70c0ef68ad22441d323d11b97e49bb234c
no1evil <notevil036@gmail.com>
2026-08-18 21:58:31 +0300

    refactor(application): UpdateUserEducationPathDto remove companyName field

--- backend/application/src/main/java/dev/tsumakov/application/profile/education/dto/in/UpdateUserEducationPathDto.java
+++ backend/application/src/main/java/dev/tsumakov/application/profile/education/dto/in/UpdateUserEducationPathDto.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 public record UpdateUserEducationPathDto(
     UUID userEducationPathId,
     String title,
-    String companyName,
     String location,
     Map<String, String> description,
     OffsetDateTime startDate,
