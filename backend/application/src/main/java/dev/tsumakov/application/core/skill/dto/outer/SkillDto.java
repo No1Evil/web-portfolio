0ad9831b6e8802107c929de1a5d6de182494c2ed
no1evil <notevil036@gmail.com>
2026-08-18 21:16:40 +0300

    feat(application): SkillDto add createdAt, updatedAt fields

--- backend/application/src/main/java/dev/tsumakov/application/core/skill/dto/outer/SkillDto.java
+++ backend/application/src/main/java/dev/tsumakov/application/core/skill/dto/outer/SkillDto.java
@@ -1,9 +1,13 @@
 package dev.tsumakov.application.core.skill.dto.outer;
 
+import java.time.OffsetDateTime;
+
 public record SkillDto(
     Integer id,
     Integer categoryId,
-    String name
+    String name,
+    OffsetDateTime createdAt,
+    OffsetDateTime updatedAt
 ) {
 
 }
