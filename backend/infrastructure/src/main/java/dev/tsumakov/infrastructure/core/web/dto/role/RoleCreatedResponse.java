681b756dfdc291f2988eeff6cd45e8fa81213bf5
no1evil <notevil036@gmail.com>
2026-08-18 15:44:41 +0300

    refactor(infrastucture): remove old implementations

--- backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/core/web/dto/role/RoleCreatedResponse.java
+++
@@ -1,8 +0,0 @@
-package dev.tsumakov.infrastructure.core.web.dto.role;
-
-public record RoleCreatedResponse(
-    Integer id,
-    String name
-) {
-
-}
--- backend/infrastructure/src/main/java/dev/tsumakov/infrastructure/core/web/dto/user/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package dev.tsumakov.infrastructure.core.web.dto.user;
-
-public record UserResponse(
-    String firstName,
-    String lastName,
-    String avatarUrl
-) {
-
-}
