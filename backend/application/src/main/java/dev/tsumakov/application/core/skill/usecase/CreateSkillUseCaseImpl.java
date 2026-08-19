6e68fe8a88a0270b8c0441ea6c9d20f4e713ee10
no1evil <notevil036@gmail.com>
2026-08-19 12:34:25 +0300

    feat(application): CreateSkillUseCaseImpl pass SkillCategoryRepository to check if category exists, and cleanup code

--- backend/application/src/main/java/dev/tsumakov/application/core/skill/usecase/CreateSkillUseCaseImpl.java
+++ backend/application/src/main/java/dev/tsumakov/application/core/skill/usecase/CreateSkillUseCaseImpl.java
@@ -4,26 +4,43 @@ import dev.tsumakov.application.core.skill.dto.in.CreateSkillDto;
 import dev.tsumakov.application.core.skill.dto.outer.SkillDto;
 import dev.tsumakov.application.core.skill.mapper.SkillDtoMapper;
 import dev.tsumakov.application.core.skill.port.in.CreateSkillUseCase;
+import dev.tsumakov.application.core.skillcategory.exception.SkillCategoryNotFoundException;
 import dev.tsumakov.domain.core.skill.factory.SkillFactory;
 import dev.tsumakov.domain.core.skill.model.Skill;
 import dev.tsumakov.domain.core.skill.repository.SkillRepository;
+import dev.tsumakov.domain.core.skillcategory.model.SkillCategory;
+import dev.tsumakov.domain.core.skillcategory.repository.SkillCategoryRepository;
 
 public class CreateSkillUseCaseImpl implements CreateSkillUseCase {
 
   private final SkillFactory skillFactory;
   private final SkillRepository skillRepository;
+  private final SkillCategoryRepository skillCategoryRepository;
   private final SkillDtoMapper skillDtoMapper;
 
   public CreateSkillUseCaseImpl(SkillFactory skillFactory, SkillRepository skillRepository,
+      SkillCategoryRepository skillCategoryRepository,
       SkillDtoMapper skillDtoMapper) {
     this.skillFactory = skillFactory;
     this.skillRepository = skillRepository;
+    this.skillCategoryRepository = skillCategoryRepository;
     this.skillDtoMapper = skillDtoMapper;
   }
 
   @Override
   public SkillDto execute(CreateSkillDto command) {
-    Skill skill = skillFactory.createNew(command.categoryId(), command.name(), command.iconUrl());
+    var category = findById(command.categoryId());
+    var skill = createSkill(category.id(), command.name(), command.iconUrl());
     return skillDtoMapper.toDto(skillRepository.create(skill));
+  }
+
+  private SkillCategory findById(Integer categoryId) {
+    return skillCategoryRepository.findById(categoryId)
+        .orElseThrow(() -> new SkillCategoryNotFoundException(
+            "Skill category with id " + categoryId + " not found"));
+  }
+
+  private Skill createSkill(Integer categoryId, String skillName, String iconUrl) {
+    return skillFactory.createNew(categoryId, skillName, iconUrl);
   }
 }
